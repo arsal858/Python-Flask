9d09c9b41c842bdb6d4a98fb48931849ab3b2efd
Muhammad Arslan Shahzad <arslanshahzadgondal@gmail.com>
2026-06-30 14:09:54 +0500

    Commited Sql Alchemy

---
+++ app7.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app=Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+
+
+db=SQLAlchemy(app)
+
+class User(db.Model):
+    id=db.Column(db.Integer(),primary_key=True)
+    name=db.Column(db.String(100))
+    date_joinded=db.Column(db.DateTime)
+def insert_data():
+    from datetime import datetime
+    new_user=User(name="Arslan Shahzad Gondal",date_joined=datetime.now())
+    db.session.add(new_user)
+
---
+++ app8.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    date_joined = db.Column(db.DateTime)

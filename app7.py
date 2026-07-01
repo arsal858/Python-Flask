6803fc0d28baeeb09e9cd743b4f64340dfbdc6a0
Muhammad Arslan Shahzad <arslanshahzadgondal@gmail.com>
2026-07-01 12:07:39 +0500

    Made Changes to the file

--- app7.py
+++ app7.py
@@ -14,8 +14,10 @@ class User(db.Model):
     id=db.Column(db.Integer(),primary_key=True)
     name=db.Column(db.String(100))
     date_joinded=db.Column(db.DateTime)
+
 def insert_data():
     from datetime import datetime
     new_user=User(name="Arslan Shahzad Gondal",date_joined=datetime.now())
     db.session.add(new_user)
+    db.session.commit()
 
